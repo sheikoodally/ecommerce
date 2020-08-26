5698b0367f124524dd88146928c39882e57e0dcd
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2020-08-26 12:17:57 -0400

    adding redux

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -1,40 +1,55 @@
 import React, {useState, useEffect} from 'react';
-import data from '../data.js'
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+//import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProduct } from '../actions/productActions';
 
 function Home(props) {
 
-    const [products, setProducts] = useState([]);
+    //const [products, setProducts] = useState([]);
+    const productList = useSelector(state => state.productList);
+    console.log(productList)
+    const {products, loading, error} = productList;
+    console.log("products: "+products);
+    console.log("loading: " + loading );
+    console.log("error: " +  error);
 
+    const dispatch = useDispatch();
     useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await axios.get("/api/products");
-            setProducts(data);
-        }
+        dispatch(listProduct());
 
-        fetchData()
-        return () => {
+        return () =>{
+            //
+        };
+    },[])
 
-        }
-    }, [])
+    //useEffect(() => {
+        //const fetchData = async () => {
+            //const {data} = await axios.get("/api/products");
+            //setProducts(data);
+        //}
+        //fetchData()
+        //return () => {
+     //   }
+    //}, [])
     return (
+    loading? <div>Loading... {console.log('loading...')}</div> : error? <div>{error}</div> :
         <div>
             <ul className="products">
 
-        {products.map( product => 
-            <li key={product.id}>
-                <div className="product">
-                    <Link to={'/product/' + product.id}><img className="product-image" src={product.image} alt={product.image}/></Link>
-                    <div className="product-name"><Link to={'/product/' + product.id}>{product.name}</Link></div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">${product.price}</div>
-                    <div className="product-rating">{product.stars} Stars ({product.reviews} Reviews)</div>
-                </div>
-            </li>
-        )}
+                {products.map( product => 
+                    <li key={product.id}>
+                        <div className="product">
+                            <Link to={'/product/' + product.id}><img className="product-image" src={product.image} alt={product.image}/></Link>
+                            <div className="product-name"><Link to={'/product/' + product.id}>{product.name}</Link></div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">${product.price}</div>
+                            <div className="product-rating">{product.stars} Stars ({product.reviews} Reviews)</div>
+                        </div>
+                    </li>
+                )}
 
-</ul>
+            </ul>
         </div>
     )
 }
---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,28 @@
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS} from '../constants/productConstants';
+import axios from 'axios';
+
+const listProduct = () => async (dispatch) => {
+    try{
+        dispatch({type: PRODUCT_LIST_REQUEST});
+        const {data} = await axios.get("/api/products");
+        dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
+    }
+    catch (error){
+        dispatch({type: PRODUCT_LIST_FAIL, payload: error.message});
+    }
+    
+}
+
+const listDetailProduct = () => async (dispatch) => {
+    try{
+        dispatch({type: PRODUCT_DETAIL_REQUEST});
+        const {data} = await axios.get("/api/products/");
+        dispatch({type: PRODUCT_DETAIL_SUCCESS, payload: data});
+    }
+    catch (error){
+        dispatch({type: PRODUCT_DETAIL_FAIL, payload: error.message});
+    }
+    
+}
+
+export {listProduct, listDetailProduct};
